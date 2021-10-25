dac18c50e9148bf1cc2b9626bc3103b9cdde6e43
René KONÉ <kone.rene@hotmail.fr>
2021-10-25 16:57:59 +0200

    Fin de la partie Stories en statique

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import Header from "../components/Header";
+import Post from "../components/Post";
+import Stories from "../components/Stories";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Stories />
+      <Post />
     </SafeAreaView>
   );
 }
